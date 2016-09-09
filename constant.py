4845bbdf13417eb6e46f3d76741693a72ace174c
git <xiaowang@qq.com>
2016-09-09 11:42:28 +0800

    初始化

---
+++ constant.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+
+TIME_CONSTANT = [u'秒', u'分钟', u'小时', u'天']
+POSTAGE_CONSTANT = ['B', 'KB', 'M', 'G', 'T']
---
+++ setup.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name="ttutils",
+    version="0.10",
+    description=u"工具类",
+    author="xw",
+    url="https://github.com/wangpanjun/ttutils.git",
+    license="LGPL",
+    packages=find_packages(),
+)
